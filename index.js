32c3414438509106ce61a76b4a009cfb88f2c161
Edav13 <edav13@hotmail.com>
2017-11-26 15:20:50 -0400

    codigoMentor 0010

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-var a = parseInt(prompt('Ingrese un valor: '));
-var b = parseInt(prompt('Ingrese un valor: '));
-var op = prompt('Ingrese un operador');
-
-switch(op) {
-    case '+':
-    document.write('la suma es: ', a + b)
-    break;
-    case '-':
-    document.write('la resta es: ', a - b)
-    break;
-    case '*':
-    document.write('la multiplicacion es: ', a * b)
-    break;
-    case '/':
-    document.write('la Division es: ', a / b)
-    break;
-    default:
-    document.write('Operador Invalido')
-    break;
+for(var i = 0; i <= 100; i++) {
+    document.write(i + ' ');
 }
