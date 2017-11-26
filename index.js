74ad373101a5bf5f469173abb60f135b10e8de54
Edav13 <edav13@hotmail.com>
2017-11-26 19:19:50 -0400

    codigoMentor 0014

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-for(var i = 1; i < 9; i++) {
-    if(i == 5) {
-        break;
-    }
-    document.write(i + ' ');
-}
-document.write('<br> ');
-
-for(var i = 1; i < 9; i++) {
-    if(i == 5) {
-        continue;
-    }
-    document.write(i + ' ');
+function miFuncion(a, b) {
+    document.write('La suma es: ', a + b);
 }
