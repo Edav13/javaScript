06e6ef88fd192be368d1311b710c8adf12609257
Edav13 <edav13@hotmail.com>
2017-11-26 19:24:26 -0400

    codigoMentor 0015

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function miFuncion(a, b) {
-    document.write('La suma es: ', a + b);
+    return a + b;
 }
+document.write('La suma es: ' + miFuncion(2, 5));
+
+var persona = function(nombre) {
+    return 'Su nombre es: ' + nombre; 
+};
+document.write('<br> ');
+
+document.write(persona('Luis'));
