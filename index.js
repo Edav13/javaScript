88e3192b4f0d015f3d2fbbae95441160ae3a28b3
Edav13 <edav13@hotmail.com>
2017-11-26 20:47:40 -0400

    codigoMentor 0025

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function miFuncion() {
-    //document.querySelector('.miLink').style.backgroundColor = 'yellow';
-    var a = document.querySelector('a[class="miLink"]');
-    a.style.backgroundColor = 'yellow';
-    a.style.border = '4px solid green';
+    var texto;
+    var num = document.getElementById('caja').value;
+    if(isNaN(num) || num < 1 || num > 8) {
+        texto = '<span style="color:red">Input invalido</span>';
+    } else {
+        texto = '<span style="color:green">Input valido</span>';
+    }
+    document.getElementById('info').innerHTML = texto;
 }
