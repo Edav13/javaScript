f0d556b676411322105f87b0253e0507f5dba230
Edav13 <edav13@hotmail.com>
2017-11-26 20:28:26 -0400

    codigoMentor 0022

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-document.write(Math.round(4.3) + '<br>');
-document.write(Math.abs(-10) + '<br>');
-document.write(Math.ceil(4.1) + '<br>');
-document.write(Math.floor(7.9) + '<br>');
-document.write(Math.pow(3, 2) + '<br>');
-document.write(Math.sqrt(3) + '<br>');
+var num = parseInt(prompt('Ingrese un valor:'));
 
-document.write(Math.random() + '<br>');
-document.write(Math.floor(Math.random() * 10) + '<br>');
+function factorial(n) {
+    return n > 1 ? n * factorial(n - 1) : 1;
+}
+
+document.write(num + ' el factorial es: ' + factorial(num))
