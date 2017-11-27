5cfc869ceea29ac3045a68c23e5a58ac1f0be2be
Edav13 <Edav13@hotmail.com>
2017-11-27 10:23:02 -0400

    codigoMentor 0035

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var cad = 'Hola mundo';
-document.getElementById('info').innerHTML = cad.slice(3);
-document.getElementById('info').innerHTML += '<br>' + cad.slice(3, 6);
+var d = new Date();
+var e = document.getElementById('info');
+e.innerHTML = d.toString();
+e.innerHTML += '<br>' + d.getTime();
+e.innerHTML += '<br>' + d.getDate();
+e.innerHTML += '<br>' + d.getDay();
+e.innerHTML += '<br>' + d.getMonth();
+e.innerHTML += '<br>' + d.getFullYear();
+
+
 
