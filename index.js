67a9415f76d4a3e157d9f08e49052cb80a643812
Edav13 <Edav13@hotmail.com>
2017-11-27 10:28:58 -0400

    codigoMentor 0036

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var d = new Date();
 var e = document.getElementById('info');
-e.innerHTML = d.toString();
-e.innerHTML += '<br>' + d.getTime();
-e.innerHTML += '<br>' + d.getDate();
-e.innerHTML += '<br>' + d.getDay();
-e.innerHTML += '<br>' + d.getMonth();
-e.innerHTML += '<br>' + d.getFullYear();
+e.innerHTML = d.getHours() > 12 ? 12 - d.getHours() : d.getHours()  + ':' + d.getMinutes() + ':' + d.getSeconds();
 
 
 
