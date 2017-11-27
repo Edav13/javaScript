87e8d86b66bee35de7660207afa0f491e2db6f77
Edav13 <edav13@hotmail.com>
2017-11-26 20:15:21 -0400

    codigoMentor 0020

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var ejecutar;
+var cont = 0;
 
 function comenzar(){
-    ejecutar = setTimeout(function() {
+    ejecutar = setInterval(function() {
         document.write('Esta es una prueba<br>');
+        cont++;
+        if(cont == 6) {
+            clearInterval(ejecutar)
+        }
     }, 3000);
 }
-
-function detenerse() {
-    clearTimeout(ejecutar);
-}
+comenzar();
